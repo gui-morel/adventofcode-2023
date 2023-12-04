ae23d5ef5c35d19a6a3ffbb8acfbfd8d4265684e
Guillaume Morel <guillaume.morel763@gmail.com>
2023-12-04 02:30:56 +0100

    Day 3 - First two

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -18,18 +18,19 @@ private val example = readDayExample(current_day)
 
 fun main() {
     println(firstPart(input))
-    println(secondPart(example))
+    println(secondPart(input))
 }
 
 private data class Position(val x: Int, val y: Int)
 private data class Schematic(val number: Int, val position: Set<Position>)
+private data class Symbol(val symbol: Char, val position: Position)
 
 private class EngineSchematicParser {
     private var currentNumber = 0;
     private var currentPosition: Set<Position> = emptySet()
 
     private var schematics = emptyList<Schematic>()
-    private var symbolPositions = emptyList<Position>()
+    private var symbols = emptyList<Symbol>()
 
     fun parse(char: Char, position: Position) {
         if (char == '.') {
@@ -38,7 +39,7 @@ private class EngineSchematicParser {
             currentNumber = currentNumber * 10 + char.digitToInt()
             currentPosition = currentPosition + position
         } else {
-            symbolPositions = symbolPositions + position
+            symbols = symbols + Symbol(char, position)
             this.end()
         }
     }
@@ -53,10 +54,20 @@ private class EngineSchematicParser {
 
     fun schematicIncluded(): Set<Schematic> {
         return schematics.filter { schematic ->
-            symbolPositions.any { symbolPosition ->
-                (adjacent(symbolPosition) intersect schematic.position).isNotEmpty()
+            symbols.any { symbol ->
+                (adjacent(symbol.position) intersect schematic.position).isNotEmpty()
             }
         }.toSet()
+    }
+
+
+    fun gears(): List<Pair<Schematic, Schematic>> {
+        return symbols.filter { symbol -> symbol.symbol == '*' }
+            .map { symbol ->
+                schematics.filter { schematic -> (adjacent(symbol.position) intersect schematic.position).isNotEmpty() }
+            }
+            .filter { adjacentSchematics -> adjacentSchematics.size == 2 }
+            .map { schematics -> schematics.first() to schematics.last() }
     }
 
     private fun adjacent(position: Position): List<Position> {
@@ -85,5 +96,12 @@ private fun firstPart(input: List<String>): Int {
 }
 
 private fun secondPart(input: List<String>): Int {
-    return -1
+    val parser = EngineSchematicParser()
+    input.mapIndexed { y, line ->
+        line.mapIndexed { x, char -> parser.parse(char, Position(x, y)) }
+        parser.end()
+    }
+    return parser.gears()
+        .map { (first, second) -> first.number * second.number }
+        .sum()
 }
